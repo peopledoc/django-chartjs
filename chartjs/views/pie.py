549622b3ad0f499742db86f765e2caaab222a45c
Bruno Bord <bruno.bord@novapost.fr>
2013-10-11 15:17:57 +0200

    donut chart

--- chartjs/views/pie.py
+++ chartjs/views/pie.py
@@ -16,3 +16,13 @@ class HighChartPieView(HighChartsView):
 
     def get_providers(self):
         return []
+
+
+class HighChartDonutView(HighChartPieView):
+    inner_size = '50%'
+
+    def get_series(self):
+        series = super(HighChartDonutView, self).get_series()
+        for serie in series:
+            serie.update({"innerSize": self.inner_size})
+        return series
