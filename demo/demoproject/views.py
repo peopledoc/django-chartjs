fa31dcc73ccc60a5f1605ed19e27f865d031262a
Robin Cherbonnier <robin.cherbonnier@ukg.com>
2022-02-04 10:15:39 +0100

    Django 3 compatibility

--- demo/demoproject/views.py
+++ demo/demoproject/views.py
@@ -1,7 +1,7 @@
 from itertools import islice
 from random import randint, shuffle
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 
 from chartjs.colors import COLORS, next_color
